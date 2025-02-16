42bd785695809d90f096a166d5f70ca11a9d39a6
심상갑 <kappa2410@gmail.com>
2025-02-17 08:47:10 +0900

    Initial Commit

---
+++ GabFreeStyle/GabFreeStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GabFreeStyleApp.swift
+//  GabFreeStyle
+//
+//  Created by Gab on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GabFreeStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
